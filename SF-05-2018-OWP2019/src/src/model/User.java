4e999db5e7e4b41135411bc8cc8dc9489497fd9d
Nikola Zoric <nikola.se.zoric@gmail.com>
2019-12-28 17:55:48 +0100

    Dodata klasa User koja predstavlja korisnika
    Unutar klase dodata enumeracija za tip korisnika

---
+++ SF-05-2018-OWP2019/src/src/model/User.java
@@ -0,0 +1,49 @@
+package src.model;
+
+import java.time.LocalDate;
+
+public class User {
+	
+	public enum UserRole{USER, ADMIN}
+	
+	private String username;
+	private String password;
+	private LocalDate registrationDate;
+	private UserRole userRole;
+	
+	public User () {}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDate getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDate registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public UserRole getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
+	
+	
+}
