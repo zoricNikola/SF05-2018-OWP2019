d47dd4391808b7051a394809b2c448053dc45768
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-04 19:58:04 +0100

    Implementirano dodavanje, izmena i brisanje korisnika u bazi

--- SF-05-2018-OWP2019/src/cinema/dao/UserDAO.java
+++ SF-05-2018-OWP2019/src/cinema/dao/UserDAO.java
@@ -1,8 +1,10 @@
 package cinema.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -86,5 +88,66 @@ public class UserDAO {
 		}
 		
 		return null;
+	}
+	
+	public static boolean addUser (User user) throws SQLException {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		try {
+			String query = "insert into Users values (?, ?, ?, ?, 1) ";
+			pstmt = connection.prepareStatement(query);
+			int index = 1;
+			pstmt.setString(index++, user.getUsername());
+			pstmt.setString(index++, user.getPassword());
+			pstmt.setDate(index++, Date.valueOf(user.getRegistrationDate()));
+			pstmt.setString(index++, user.getUserRole().toString());
+			
+			return pstmt.executeUpdate() == 1;
+			
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
+	}
+	
+	public static boolean updateUser (User user) throws SQLException {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		try {
+			String query = "update Users set Username = ?, Password = ?, RegistrationDate = ?, "
+					+ "Role = ? where rowid = ?";
+			pstmt = connection.prepareStatement(query);
+			int index = 1;
+			pstmt.setString(index++, user.getUsername());
+			pstmt.setString(index++, user.getPassword());
+			pstmt.setDate(index++, Date.valueOf(user.getRegistrationDate()));
+			pstmt.setString(index++, user.getUserRole().toString());
+			pstmt.setInt(index++, user.getId());
+			
+			return pstmt.executeUpdate() == 1;
+			
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
+	}
+	
+	public static boolean deleteUser (User user) throws SQLException {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		try {
+			String query = "update Users set Active = 0 where rowid = ?";
+			pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1, user.getId());
+			
+			return pstmt.executeUpdate() == 1;
+			
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
 	}
 }
