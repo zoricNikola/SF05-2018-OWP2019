0e2f36a73f2c8645c6f4de325873a7d7dbb8e007
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-04 19:25:13 +0100

    Implementirano preuzimanje pojedinog filma i korisnika iz baze
    U MovieDAO i UserDAO klasama dodate metode getMovieByID i
    getUserByUsername

--- SF-05-2018-OWP2019/src/cinema/dao/MovieDAO.java
+++ SF-05-2018-OWP2019/src/cinema/dao/MovieDAO.java
@@ -62,4 +62,47 @@ public class MovieDAO {
 		
 		return movies;
 	}
+	
+	public static Movie getMovieByID (int mID) throws Exception {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		try {
+			String query = "select * from Movies where Active = 1 and ID = ? ";
+			pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1, mID);
+			System.out.println(pstmt);
+			
+			rset = pstmt.executeQuery();
+			
+			if (rset.next()) {
+				int index = 1;
+				int id = rset.getInt(index++);
+				String title = rset.getString(index++);
+				String director = rset.getString(index++);
+				int duration = rset.getInt(index++);
+				String distributor = rset.getString(index++);
+				String country = rset.getString(index++);
+				int year = rset.getInt(index++);
+				String description = rset.getString(index++);
+				boolean active = rset.getInt(index++) == 1;
+				
+				List<String> actors = ActorDAO.getByMovieID(id);
+				List<String> genres = GenreDAO.getByMovieID(id);
+				
+				return new Movie(id, title, director, actors, genres, duration, 
+						distributor, country, year, description, active);
+			}
+			
+					
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { rset.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
+		
+		return null;
+	}
 }
--- SF-05-2018-OWP2019/src/cinema/dao/UserDAO.java
+++ SF-05-2018-OWP2019/src/cinema/dao/UserDAO.java
@@ -52,4 +52,37 @@ public class UserDAO {
 		
 		return users;
 	}
+	
+	public static User getUserByUsername (String UUsername) throws Exception {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		try {
+			String query = "select * from Users where Active = 1 and Username = ?";
+			pstmt = connection.prepareStatement(query);
+			pstmt.setString(1, UUsername);
+			System.out.println(pstmt);
+			
+			rset = pstmt.executeQuery();
+			
+			if (rset.next()) {
+				int index = 1;
+				String username = rset.getString(index++);
+				String password = rset.getString(index++);
+				LocalDate registrationDate = LocalDate.parse(rset.getString(index++));
+				User.UserRole userRole = User.UserRole.valueOf(rset.getString(index++));
+				boolean active = rset.getInt(index++) == 1;
+				
+				return new User(username, password, registrationDate, userRole, active);
+			}
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { rset.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
+		
+		return null;
+	}
 }
