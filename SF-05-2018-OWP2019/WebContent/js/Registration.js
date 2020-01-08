55ecba34766a1427aec62f5b1ee30a55076b451f
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-08 22:11:13 +0100

    Kreirana stranica za registrovanje
    Takođe, kreirana skripta koja je vezana za ovu stranicu koja za sada
    preuzima unete podatke i vrši validaciju istih

---
+++ SF-05-2018-OWP2019/WebContent/js/Registration.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	var usernameInput = $('#usernameInput');
+	var passwordInput = $('#passwordInput');
+	var repeatedPasswordInput = $('#repeatedPasswordInput');
+	
+	var messageParagraph = $('#messageParagraph');
+	
+	$('#registrationSubmit').on('click', function(event) {
+		event.preventDefault();
+		
+		var username = usernameInput.val().trim();
+		var password = passwordInput.val().trim();
+		var repeatedPassword = repeatedPasswordInput.val().trim();
+		console.log('Username: ' + username);
+		console.log('Password: ' + password);
+		console.log('Repeated password: ' + repeatedPassword);
+		
+		if (username === '') {
+			messageParagraph.text('Korisničko ime mora biti uneto!');
+		}
+		else if (password === '') {
+			messageParagraph.text('Lozinka mora biti uneta!');
+		}
+		else if (repeatedPassword === '') {
+			messageParagraph.text('Ponovljena lozinka mora biti uneta!');
+		}
+		else if (password != repeatedPassword) {
+			messageParagraph.text('Lozinke nisu iste!');
+		}
+		else if (username.match(/^[0-9]+$/)) {
+			messageParagraph.text('Korisničko ime se ne može sastojaeti samo od brojeva!');	
+		}
+		else if (!username.match(/^[0-9a-z]+$/i)) {
+			messageParagraph.text('Korisničko ime se može sastojaeti samo od slova i brojeva!');
+		}
+		else if (!password.match(/^[0-9a-z]+$/i)) {
+			messageParagraph.text('Lozinka se može sastojaeti samo od slova i brojeva!');
+		}
+		else {
+			messageParagraph.text('');
+			var params = {
+					'username': username,
+					'password': password
+			}
+			
+		}
+		
+		return false;
+	});
+});
