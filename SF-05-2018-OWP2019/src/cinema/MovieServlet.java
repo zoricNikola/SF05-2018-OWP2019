d6385cebd77747d764c84385421688437aea916a
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-14 02:40:07 +0100

    MovieServlet sada za prosleđen id vraća film

--- SF-05-2018-OWP2019/src/cinema/MovieServlet.java
+++ SF-05-2018-OWP2019/src/cinema/MovieServlet.java
@@ -1,21 +1,38 @@
 package cinema;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cinema.dao.MovieDAO;
+import cinema.model.Movie;
+
 @SuppressWarnings("serial")
 public class MovieServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String stringID = request.getParameter("id");
+		try {
+			int id = Integer.parseInt(stringID);
+			Movie movie = MovieDAO.getMovieByID(id);
+			
+			Map<String, Object> data = new LinkedHashMap<String, Object>();
+			data.put("movie", movie);
+			
+			request.setAttribute("data", data);
+			request.getRequestDispatcher("./SuccessServlet").forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
