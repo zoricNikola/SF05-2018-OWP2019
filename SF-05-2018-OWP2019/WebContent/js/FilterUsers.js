b88d11b698131a35f0b5c1e9b3bfe7b02cd29786
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-15 01:09:57 +0100

    Dovršen prikaz svih korisnika sa filtriranjem i sortiranjem

--- SF-05-2018-OWP2019/WebContent/js/FilterUsers.js
+++ SF-05-2018-OWP2019/WebContent/js/FilterUsers.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
-
+	
     var app = new Vue({
         el: '#app',
         data: {
             username: '',
-            userRole: '',
-            users: []
+            users: [],
+            selectedSort: '',
+            sortOptions: [
+            	{ text: 'Sortiraj...', value: ''},
+            	{ text: 'Automatski', value: ''},
+            	{ text: 'Korisničko ime a...z', value: 'usernameASC'},
+            	{ text: 'Korisničko ime z...a', value: 'usernameDESC'},
+            	{ text: 'Uloga a...z', value: 'userRoleASC'},
+            	{ text: 'Uloga z...a', value: 'userRoleDESC'}
+            ],
+            selectedUserRole: '',
+            userRoleOptions: [
+            	{ text: 'Uloga', value: ''},
+            	{ text: 'Sve', value: ''},
+            	{ text: 'Korisnik', value: 'USER' },
+            	{ text: 'Admin', value: 'ADMIN' }
+            ],
         },
         methods: {
             getUsers: function(event) {
                 console.log('username: ' + this.username);
-                console.log('userRole: ' + this.userRole);
+                console.log('userRole: ' + this.selectedUserRole);
 
                 var params = {
                     'username': this.username,
-                    'userRole': this.userRole
+                    'userRole': this.selectedUserRole
                 }
 
                 $.get('FilterUsersServlet', params, function(data) {
@@ -24,12 +39,49 @@ $(document).ready(function() {
                     
                 });
             }
+//            refresh: function(event) {
+//            	this.getUsers();
+//            	event.preventDefault();
+//        		return false;
+//            }
         },
         computed: {
             orderedUsers: function () {
-                return this.users;
+            	if (this.selectedSort === '')
+            		return this.users;
+            	else if (this.selectedSort === 'usernameASC')
+            		return _.orderBy(this.users, 'username');
+            	else if (this.selectedSort === 'usernameDESC')
+            		return _.orderBy(this.users, 'username').reverse();
+            	else if (this.selectedSort === 'userRoleASC')
+            		return _.orderBy(this.users, 'userRole');
+            	else if (this.selectedSort === 'userRoleDESC')
+            		return _.orderBy(this.users, 'userRole').reverse();
               }
         }
     })
+    
+    $('#usernameInput').on('keyup', function(event) {
+		app.getUsers();
+
+		event.preventDefault();
+		return false;
+	});
+    
+    $('#userRoleInput').on('change', function (event) {
+        app.getUsers();
+        
+        event.preventDefault();
+        return false;
+        
+    });
+    $('#sortInput').on('change', function (event) {
+        
+        
+        event.preventDefault();
+        return false;
+        
+    });
+    
     app.getUsers();
 });
