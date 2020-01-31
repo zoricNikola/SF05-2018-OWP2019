aa5f8a0581f7de878e9a85655b125fb5968cacb2
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-31 18:02:04 +0100

    Kreiran LoginServlet
    Ovaj servlet prihvata uneto korisničko ime i lozinku i ukoliko su uneti
    ispravni podaci, u trenutnu sesiju se smešta korisničko ime prijavljenog
    korisnika

---
+++ SF-05-2018-OWP2019/src/cinema/LoginServlet.java
@@ -0,0 +1,57 @@
+package cinema;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cinema.dao.UserDAO;
+import cinema.model.User;
+
+@SuppressWarnings("serial")
+public class LoginServlet extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		try {
+			User user = UserDAO.getUserByUsername(username);
+			
+			if (user == null) {
+				throw new Exception("Pogrešno korisničko ime!");
+			}
+			if (!user.getPassword().equals(password)) {
+				throw new Exception("Pogrešna lozinka!");
+			}
+			
+			request.getSession().setAttribute("loggedInUsername", user.getUsername());
+			request.getRequestDispatcher("./SuccessServlet").forward(request, response);
+			
+		} catch (Exception e) {
+			String message = e.getMessage();
+			if (message == null) {
+				message = "Nepredvidjena greska!";
+				e.printStackTrace();
+			}
+			
+			Map<String, Object> data = new LinkedHashMap<String, Object>();
+			data.put("message", message);
+			
+			request.setAttribute("data", data);
+			request.getRequestDispatcher("./FailureServlet").forward(request, response);
+			
+		}
+	}
+
+}
