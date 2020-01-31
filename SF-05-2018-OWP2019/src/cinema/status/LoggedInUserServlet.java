eff704e9ab71e3b6792572159e4362c519b5521f
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-01-31 19:30:22 +0100

    Kreirani novi statusni servleti
    UnauthorizedServlet - kada korisnik nije prijavljen, a pokušava da
    pristupi delu sistema koji od njega zahteva da bude prijavljen zahtev se
    prosleđuje ovom servletu
    
    LogoutServlet - koristi se za brisanje trenutne sesije, odnosno za
    odjavu sa sistema
    
    LoggedInUserServlet - koristi se da bi se utvrdilo da li je korisnik
    ulogovan i ako jeste vraća dodatno ulogu tog korisnika

---
+++ SF-05-2018-OWP2019/src/cinema/status/LoggedInUserServlet.java
@@ -0,0 +1,54 @@
+package cinema.status;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cinema.dao.UserDAO;
+import cinema.model.User;
+import cinema.model.User.UserRole;
+import cinema.searchModels.UsersSearchModel;
+
+@SuppressWarnings("serial")
+public class LoggedInUserServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String loggedInUsername = (String) request.getSession().getAttribute("loggedInUsername");
+		if (loggedInUsername == null) {
+			request.getRequestDispatcher("./LogoutServlet").forward(request, response);
+			return;
+		}
+		try {
+			
+			User loggedInUser = UserDAO.getUserByUsername(loggedInUsername);
+			if (loggedInUser == null) {
+				request.getRequestDispatcher("./LogoutServlet").forward(request, response);
+				return;
+			}
+			
+			String userRole = request.getParameter("userRole");
+			
+			Map<String, Object> data = new LinkedHashMap<String, Object>();
+			data.put("loggedInUserRole", userRole);
+			
+			request.setAttribute("data", data);
+			request.getRequestDispatcher("./SuccessServlet").forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.getRequestDispatcher("./FailureServlet").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
