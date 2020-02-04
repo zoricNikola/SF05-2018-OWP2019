d6ffb9db6fad148382b09040ccf6384755fce1fe
Nikola Zoric <nikola.se.zoric@gmail.com>
2020-02-05 00:24:56 +0100

    Implementirano dodavanje i brisanje projekcija

--- SF-05-2018-OWP2019/src/cinema/dao/ProjectionDAO.java
+++ SF-05-2018-OWP2019/src/cinema/dao/ProjectionDAO.java
@@ -6,7 +6,9 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import cinema.model.Movie;
 import cinema.model.Projection;
+import cinema.model.User;
 import cinema.searchModels.ProjectionsSearchModel;
 import cinema.util.DateTimeUtil;
 
@@ -98,6 +100,46 @@ public class ProjectionDAO {
 		}
 		
 		return null;
+	}
+	
+	public static boolean addProjection (Projection projection) throws Exception {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		try {
+			String query = "insert into Projections (Movie, ProjectionType, Hall, Time, Price, Admin, Active) values (?, ?, ?, ?, ?, ?, 1) ";
+			pstmt = connection.prepareStatement(query);
+			int index = 1;
+			pstmt.setInt(index++, projection.getMovie().getId());
+			pstmt.setInt(index++, projection.getProjectionType().getId());
+			pstmt.setInt(index++, projection.getHall().getId());
+			pstmt.setInt(index++, DateTimeUtil.LocalDateTimeToUnixTimeStamp(projection.getTime()));
+			pstmt.setDouble(index++, projection.getPrice());
+			pstmt.setString(index++, projection.getAdmin().getUsername());
+			
+			return pstmt.executeUpdate() == 1;
+			
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
+	}
+	
+	public static boolean deleteProjection (Projection projection) throws Exception {
+		Connection connection = ConnectionManager.getConnection();
+		
+		PreparedStatement pstmt = null;
+		try {
+			String query = "update Projections set Active = 0 where ID = ?";
+			pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1, projection.getId());
+			
+			return pstmt.executeUpdate() == 1;
+			
+		} finally {
+			try { pstmt.close(); } catch (Exception e1) { e1.printStackTrace(); }
+			try { connection.close(); } catch (Exception e1) { e1.printStackTrace(); }
+		}
 	}
 
 }
